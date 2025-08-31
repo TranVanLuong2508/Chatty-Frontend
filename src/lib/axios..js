8af2afde7d83b4dc399130c6ab3bf01cdda6d4b9
Tran Van Luong <tranvanluong032020@gmail.com>
2025-08-31 15:08:10 +0700

    page: sign up

--- src/lib/axios..js
+++ src/lib/axios..js
@@ -1,6 +1,15 @@
 import axios from "axios";
 
-export const exiosInstance = axios.create({
+const axiosInstance = axios.create({
     baseURL: "http://localhost:5001/api",
     withCredentials: true
 })
+
+// axiosInstance.interceptors.response.use(
+//     (response) => {
+//         // const { data } = response
+//         return response.data
+//     }
+// );
+
+export { axiosInstance }
